e2ffe5acfdaca966e0b3038db941805b4178f6f7
Affan <shaikabdulla0298@gmail.com>
2025-06-19 17:33:44 +0530

    Post_Method Put_Method handled

--- src/main/java/com/student/controller/StudentRestController.java
+++ src/main/java/com/student/controller/StudentRestController.java
@@ -5,10 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.PutExchange;
 
 import com.student.dto.StudentRequest;
 import com.student.dto.StudentResponse;
@@ -25,6 +29,15 @@ public class StudentRestController {
 	public ResponseEntity<StudentResponse> createStudent(@RequestBody StudentRequest req) {
 		List<StudentResponse> response = studentService.createStudent(req);
 		return new ResponseEntity(response, HttpStatus.CREATED);
+
+	}
+
+	@PutMapping("/api/updateStudent/{id}")
+	public ResponseEntity<List<StudentResponse>> updateStudent(@PathVariable String id, @RequestBody StudentRequest req) {
+		req.setId(id);
+
+		List<StudentResponse> response = studentService.updateStudent(req);
+		return ResponseEntity.ok(response);
 
 	}
 
